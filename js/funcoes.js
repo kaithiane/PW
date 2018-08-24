b1b32d4ae0e648d3e0109866391bafe2bd737382
kaithiane <kaithiane@hotmail.com>
2018-08-23 22:30:47 -0300

    abrir janela de calculo

--- js/funcoes.js
+++ js/funcoes.js
@@ -23,5 +23,6 @@ function mostrarElemento() {
 }
 
 function calcularElemento() {
-	document.getElementById("demo").innerHTML = parseFloat(document.getElementById("num1").value) + parseFloat(document.getElementById("num2").value);
+	var total = parseFloat(document.getElementById("num1").value) + parseFloat(document.getElementById("num2").value);
+	window.alert(total);
 }
